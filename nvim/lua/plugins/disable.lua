87d6825730b9fbf2831d2c83be72c62ac1b6d9fd
Darren Armstrong <darren.armstrong0@gmail.com>
2023-08-19 21:17:52 +0800

    batman

---
+++ nvim/lua/plugins/disable.lua
@@ -0,0 +1,10 @@
+return {
+  -- disable neotree
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+}
